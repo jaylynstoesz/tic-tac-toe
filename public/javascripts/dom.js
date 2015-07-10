606c31e7eaa61023047fe749be15bb92e8bdbf7c
Roberto Ortega <berto.ort@gmail.com>
2015-07-10 14:07:27 -0600

    all done

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -13,6 +13,9 @@ window.onload = function() {
   var player1 = {};
   var player2 = {};
   var win1, win2;
+  var div = document.getElementsByClassName('cell');
+  var player = document.getElementById('player');
+  var filled = [];
 
   function score() {
     for (var i = 0; i < combos.length; i++) {
@@ -28,9 +31,6 @@ window.onload = function() {
     }
   }
 
-  var div = document.getElementsByClassName('cell');
-  var player = document.getElementById('player');
-  var filled = [];
   for(var i = 0; i < div.length; i++){
     div[i].addEventListener('click', function(e){
       filled.push(e.currentTarget.id);
@@ -52,24 +52,32 @@ window.onload = function() {
       alert("Cat's Game! Click OK to start over.");
       player1 = {};
       player2 = {};
+      filled = [];
+      player.innerHTML = "player 1";
       for (var i = 0; i < div.length; i++) {
         div[i].innerHTML = "";
       }
     }
     else if (win1) {
       alert("Player 1 kicked your ass!");
+      player1 = {};
+      player2 = {};
+      filled = [];
+      player.innerHTML = "player 1";
+      win1 = false;
       for (var j = 0; j < div.length; j++) {
         div[j].innerHTML = "";
       }
-      player1 = {};
-      player2 = {};
     } else if (win2) {
       alert("Player 2 pwned your ass!");
+      player1 = {};
+      player2 = {};
+      filled = [];
+      player.innerHTML = "player 1";
+      win2 = false;
       for (var k = 0; k < div.length; k++) {
         div[k].innerHTML = "";
       }
-      player1 = {};
-      player2 = {};
     }
     });
   }
